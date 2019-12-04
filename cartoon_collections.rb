75131c2815828798960a28393ad14540f479bb75
James Rogers <jarogers095@gmail.com>
2019-12-04 19:21:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(aspects)
-  # Your code here
+  aspects.map { |aspect| "#{aspect.capitalize}!"
 end
 
 def long_planeteer_calls(calls)
