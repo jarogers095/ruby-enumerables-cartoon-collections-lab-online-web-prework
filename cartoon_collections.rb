8e9a620b84ca7774294c91e6d27b235de465165c
James Rogers <jarogers095@gmail.com>
2019-12-04 19:17:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf|
+    puts 
+end
+
+def summon_captain_planet(aspects)
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
